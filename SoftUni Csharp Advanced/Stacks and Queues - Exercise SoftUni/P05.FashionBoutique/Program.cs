842ab15da2914e89212027862edbc10bd1097fb2
Radoslav Gervaziev <radi7275@gmail.com>
2022-05-18 19:35:09 +0300

    Uploading solutions

--- SoftUni Csharp Advanced/Stacks and Queues - Exercise SoftUni/P05.FashionBoutique/Program.cs
+++ SoftUni Csharp Advanced/Stacks and Queues - Exercise SoftUni/P05.FashionBoutique/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Collections.Generic;
 
 namespace P05.FashionBoutique
@@ -7,7 +8,32 @@ namespace P05.FashionBoutique
     {
         static void Main()
         {
+            int[] clothes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 
+            Stack<int> clothesStack = new Stack<int>(clothes);
+
+            int capacity = int.Parse(Console.ReadLine());
+
+            int racks = 1;
+            int sum = 0;
+
+            while (clothesStack.Count > 0)
+            {
+                sum += clothesStack.Peek();
+
+                if (sum <= capacity)
+                {
+                    clothesStack.Pop();
+                }
+                else
+                {
+                    racks++;
+                    sum = 0;
+                }
+                
+            }
+
+            Console.WriteLine(racks);
         }
     }
 }
---
+++ SoftUni Csharp Advanced/Stacks and Queues - Exercise SoftUni/P06.SongsQueue/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P06.SongsQueue
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
