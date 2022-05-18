1ba8dd54a13646a39f704c81a3dbf98c76096c9b
Radoslav Gervaziev <radi7275@gmail.com>
2022-05-18 20:17:30 +0300

    P06 solved

--- SoftUni Csharp Advanced/Stacks and Queues - Exercise SoftUni/P06.SongsQueue/Program.cs
+++ SoftUni Csharp Advanced/Stacks and Queues - Exercise SoftUni/P06.SongsQueue/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
 
 namespace P06.SongsQueue
@@ -8,7 +7,35 @@ namespace P06.SongsQueue
     {
         static void Main()
         {
+            string[] songs = Console.ReadLine().Split(", ");
 
+            Queue<string> songsQueue = new Queue<string>(songs);
+
+            while (songsQueue.Count > 0)
+            {
+                string command = Console.ReadLine();
+                if (command == "Play")
+                {
+                    songsQueue.Dequeue();
+                }
+                else if (command.Substring(0, 3) == "Add")
+                {
+                    string songName = command.Substring(4);
+                    if (!songsQueue.Contains(songName))
+                    {
+                        songsQueue.Enqueue(songName);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{songName} is already contained!");
+                    }  
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", songsQueue));
+                }
+            }
+            Console.WriteLine("No more songs!");
         }
     }
 }
---
+++ SoftUni Csharp Advanced/Stacks and Queues - Exercise SoftUni/P07.TruckTour/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P07.TruckTour
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            long numberOfPumps = long.Parse(Console.ReadLine());
+
+            for (int i = 0; i < numberOfPumps; i++)
+            {
+                long[] quantityAndDistance = Console.ReadLine().Split().Select(long.Parse).ToArray();
+
+
+            }
+        }
+    }
+}
