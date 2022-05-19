6c43f0d9d777507766854e0db35bb2cc72f8ded8
Radoslav Gervaziev <radi7275@gmail.com>
2022-05-19 23:19:40 +0300

    .

--- SoftUni Csharp Advanced/Multidimensional Arrays - Exercise SoftUni/P01.DiagonalDifference/Program.cs
+++ SoftUni Csharp Advanced/Multidimensional Arrays - Exercise SoftUni/P01.DiagonalDifference/Program.cs
@@ -1,12 +1,35 @@
 ﻿using System;
+using System.Linq;
 
 namespace P01.DiagonalDifference
 {
-    internal class Program
+    class Program
     {
         static void Main()
         {
+            int matrixSize = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[matrixSize, matrixSize];
 
+            for (int row = 0; row < matrixSize; row++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                for (int column = 0; column < matrixSize; column++)
+                {
+                    matrix[row, column] = numbers[column];
+                }
+            }
+
+            int primaryDiagSum = 0;
+            int secondaryDiagSum = 0;
+
+            for (int rows = 0; rows < matrixSize; rows++)
+            {
+                for (int cols = 0; cols < matrixSize; cols++)
+                {
+                    primaryDiagSum += matrix[rows, cols];
+                }
+            }
+            Console.WriteLine(primaryDiagSum);
         }
     }
 }
