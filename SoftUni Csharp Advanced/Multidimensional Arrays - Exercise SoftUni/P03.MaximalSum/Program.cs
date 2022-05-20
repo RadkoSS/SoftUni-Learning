5f88a7678f45072b6d55b6d62e45808846b50df3
Radoslav Gervaziev <radi7275@gmail.com>
2022-05-20 14:02:14 +0300

    Solved P03.MaximalSum

--- SoftUni Csharp Advanced/Multidimensional Arrays - Exercise SoftUni/P03.MaximalSum/Program.cs
+++ SoftUni Csharp Advanced/Multidimensional Arrays - Exercise SoftUni/P03.MaximalSum/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Text;
 
 namespace P03.MaximalSum
 {
@@ -15,40 +16,6 @@ namespace P03.MaximalSum
 
             PrintSumAndSquare(matrix, matrixDimensions[0], matrixDimensions[1]);
         }
-
-        static void PrintSumAndSquare(int[,] matrix, int rowsCount, int columnsCount)
-        {
-            long maximalSum = long.MinValue;
-            int[,] biggest3x3Square = new int[3, 3];
-
-            for (int row = 0; row < rowsCount - 2; row++)
-            {
-                for (int column = 0; column < columnsCount - 2; column++)
-                {
-                    long currentSquareSum = matrix[row, column] + matrix[row, column + 1] + matrix[row, column + 2] + matrix[row + 1, column] + matrix[row + 1, column + 1] + matrix[row + 1, column + 2] + matrix[row + 2, column] + matrix[row + 2, column + 1] + matrix[row + 2, column + 2];
-                    if (maximalSum < currentSquareSum)
-                    {
-                        maximalSum = currentSquareSum;
-                        int currentRow = row;
-                        int currentCol = column;
-                        for (int squareRow = 0; squareRow < 3; squareRow++)
-                        {
-                            for (int squareColumn = 0; squareColumn < 3; squareColumn++)
-                            {
-                                biggest3x3Square[squareRow, squareColumn] = matrix[currentRow, currentCol];
-                                currentCol++;
-                            }
-                            currentRow++;
-                        }
-                    }
-                }
-            }
-
-            Console.WriteLine($"Sum = {maximalSum}");
-
-
-        }
-
         static void InitializeMatrix(int[,] matrix, int rowsCount)
         {
             for (int row = 0; row < rowsCount; row++)
@@ -60,6 +27,34 @@ namespace P03.MaximalSum
                     matrix[row, column] = numbers[column];
                 }
             }
+        }
+
+        static void PrintSumAndSquare(int[,] matrix, int rowsCount, int columnsCount)
+        {
+            long maximalSum = long.MinValue;
+            string maximal3x3Square = string.Empty;
+
+            for (int row = 0; row < rowsCount - 2; row++)
+            {
+                for (int column = 0; column < columnsCount - 2; column++)
+                {
+                    long currentSquareSum = matrix[row, column] + matrix[row, column + 1] + matrix[row, column + 2] + matrix[row + 1, column] + matrix[row + 1, column + 1] + matrix[row + 1, column + 2] + matrix[row + 2, column] + matrix[row + 2, column + 1] + matrix[row + 2, column + 2];
+                    
+                    if (maximalSum < currentSquareSum)
+                    {
+                        maximalSum = currentSquareSum;
+
+                        StringBuilder square3x3 = new StringBuilder();
+                        square3x3.AppendLine($"{matrix[row, column]} {matrix[row, column + 1]} {matrix[row, column + 2]}");
+                        square3x3.AppendLine($"{matrix[row + 1, column]} {matrix[row + 1, column + 1]} {matrix[row + 1, column + 2]}");
+                        square3x3.AppendLine($"{matrix[row + 2, column]} {matrix[row + 2, column + 1]} {matrix[row + 2, column + 2]}");
+
+                        maximal3x3Square = square3x3.ToString();
+                    }
+                }
+            }
+            Console.WriteLine($"Sum = {maximalSum}");
+            Console.WriteLine(maximal3x3Square);
         }
     }
 }
