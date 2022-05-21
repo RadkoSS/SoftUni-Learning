07ff7f91ada8ece73feab409a2524e3e86d4e985
Radoslav Gervaziev <radi7275@gmail.com>
2022-05-21 14:37:55 +0300

    Solved P05.SnakeMoves

--- SoftUni Csharp Advanced/Multidimensional Arrays - Exercise SoftUni/P05.SnakeMoves/Program.cs
+++ SoftUni Csharp Advanced/Multidimensional Arrays - Exercise SoftUni/P05.SnakeMoves/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace P05.SnakeMoves
 {
@@ -6,7 +7,60 @@ namespace P05.SnakeMoves
     {
         static void Main()
         {
+            int[] matrixDimensions = ReadArray();
 
+            char[,] matrix = new char[matrixDimensions[0], matrixDimensions[1]];
+
+            InitializeMatrix(matrix);
+
+            PrintMatrix(matrix);
+        }
+        static int[] ReadArray()
+        {
+            return Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        }
+        static void InitializeMatrix(char[,] matrix)
+        {
+            string word = Console.ReadLine();
+
+            for (int row = 0, symbol = -1; row < matrix.GetLength(0); row++)
+            {
+                if (row % 2 == 0)
+                {
+                    for (int column = 0; column < matrix.GetLength(1); column++)
+                    {
+                        if (symbol == word.Length - 1)
+                        {
+                            symbol = -1;
+                        }
+                        symbol++;
+                        matrix[row, column] = word[symbol];
+                    }
+                }
+                else
+                {
+                    for (int column = matrix.GetLength(1) - 1; column >= 0; column--)
+                    {
+                        if (symbol == word.Length - 1)
+                        {
+                            symbol = -1;
+                        }
+                        symbol++;
+                        matrix[row, column] = word[symbol];
+                    }
+                }
+            }
+        }
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int column = 0; column < matrix.GetLength(1); column++)
+                {
+                    Console.Write(matrix[row, column]);
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
