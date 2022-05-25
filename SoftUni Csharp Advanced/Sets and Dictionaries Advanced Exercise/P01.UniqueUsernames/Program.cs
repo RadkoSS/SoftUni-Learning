57056d86740a7a12a686b5a163805e6912cc11dd
Radoslav Gervaziev <radi7275@gmail.com>
2022-05-25 23:01:14 +0300

    Uploading solutions

---
+++ SoftUni Csharp Advanced/Sets and Dictionaries Advanced Exercise/P01.UniqueUsernames/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P01.UniqueUsernames
+{
+    class Program
+    {
+        static void Main()
+        {
+            int usernamesCount = int.Parse(Console.ReadLine());
+
+            HashSet<string> usernames = new HashSet<string>();
+
+            for (int i = 0; i < usernamesCount; i++)
+            {
+                usernames.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, usernames));
+        }
+    }
+}
---
+++ SoftUni Csharp Advanced/Sets and Dictionaries Advanced Exercise/P02.SetsOfElements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P02.SetsOfElements
+{
+    class Program
+    {
+        static void Main()
+        {
+            int[] lenghts = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            HashSet<int> firstSet = new HashSet<int>();
+            HashSet<int> secondSet = new HashSet<int>();
+
+            for (int i = 0; i < lenghts[0]; i++)
+            {
+                firstSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < lenghts[1]; i++)
+            {
+                secondSet.Add(int.Parse(Console.ReadLine()));
+            }
+
+            HashSet<int> resultSet = new HashSet<int>();
+
+            foreach (int number in firstSet)
+            {
+                if (firstSet.Contains(number) && secondSet.Contains(number))
+                {
+                    resultSet.Add(number);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', resultSet));
+        }
+    }
+}
