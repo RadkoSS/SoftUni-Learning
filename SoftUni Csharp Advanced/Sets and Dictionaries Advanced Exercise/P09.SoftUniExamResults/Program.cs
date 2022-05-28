a220f335265a623e387b8aaa44df8fd5b6f4c828
Radoslav Gervaziev <radi7275@gmail.com>
2022-05-28 15:25:33 +0300

    Uploading solutions

---
+++ SoftUni Csharp Advanced/Sets and Dictionaries Advanced Exercise/P09.SoftUniExamResults/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace P09.SoftUniExamResults
+{
+    class Program
+    {
+        static void Main()
+        {
+            Dictionary<string, Dictionary<string, int>> results = new Dictionary<string, Dictionary<string, int>>();
+
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "exam finished")
+            {
+                string[] commandArguments = command.Split('-');
+                string username = commandArguments[0];
+
+                if (commandArguments.Length == 3)
+                {
+                    string language = commandArguments[1];
+                    int points = int.Parse(commandArguments[2]);
+
+                    if (!results.ContainsKey(language))
+                    {
+                        results.Add(language, new Dictionary<string, int>());
+                    }
+                    else
+                    {
+                        results[language].Add(username, points);
+                    }
+                }
+                else if (commandArguments.Length == 2)
+                {
+                    results[username].TryGetValue(username, out int points);
+                }
+            }
+        }
+    }
+}
