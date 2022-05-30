3a04a118a27c46e20c5087b43a7aca3ca7855da1
Radoslav Gervaziev <radi7275@gmail.com>
2022-05-30 20:33:55 +0300

    P03. Solved

--- SoftUni Csharp Advanced/Streams, Files and Directories - Exercise SoftUni/P03.CopyBinaryFile/CopyBinaryFile.cs
+++ SoftUni Csharp Advanced/Streams, Files and Directories - Exercise SoftUni/P03.CopyBinaryFile/CopyBinaryFile.cs
@@ -1,7 +1,7 @@
 ﻿namespace CopyBinaryFile
 {
-    using System;
     using System.IO;
+    using System.Linq;
 
     public class CopyBinaryFile
     {
@@ -15,6 +15,28 @@
 
         public static void CopyFile(string inputFilePath, string outputFilePath)
         {
+
+            using (BinaryReader input = new BinaryReader(File.Open(inputFilePath, FileMode.Open)))
+            {
+
+                using (BinaryWriter output = new BinaryWriter(File.Open(outputFilePath, FileMode.OpenOrCreate)))
+                {
+                    byte[] buffer = new byte[1024];
+
+                    int bytesRead = 0;
+
+                    while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        if (bytesRead < buffer.Length)
+                        {
+                            buffer = buffer.Take(bytesRead).ToArray();
+                        }
+
+                        output.Write(buffer, 0, bytesRead);
+                    }
+                }
+
+            }
 
         }
     }
