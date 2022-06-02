f061f77f6f3f8c15455c5eafe272c5ca569090f8
Radoslav Gervaziev <radi7275@gmail.com>
2022-06-02 19:01:56 +0300

    Uploading solutions

---
+++ SoftUni Csharp Advanced/Functional Programming Exercise/P01.ActionPrint/ActionPrint.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P01.ActionPrint
+{
+    class ActionPrint
+    {
+        static void Main()
+        {
+            string[] names = Console.ReadLine().Split();
+
+            Action<string> print = name => Console.WriteLine(name);
+
+            Array.ForEach(names, print);
+        }
+    }
+}
