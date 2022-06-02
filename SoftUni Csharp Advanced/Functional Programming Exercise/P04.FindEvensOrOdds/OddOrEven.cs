9e72e4e3e27da4645c3f36db2711ce37494f1d4f
Radoslav Gervaziev <radi7275@gmail.com>
2022-06-02 21:32:01 +0300

    Uploading solution for P05.

---
+++ SoftUni Csharp Advanced/Functional Programming Exercise/P04.FindEvensOrOdds/OddOrEven.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace P04.FindEvensOrOdds
+{
+    class OddOrEven
+    {
+        static void Main()
+        {
+            int[] bordersOfArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int start = bordersOfArray[0];
+            int end = bordersOfArray[1];
+
+            int[] numbers = new int[end - start + 1];
+
+            for (int number = start, index = 0; number <= end; number++, index++)
+            {
+                numbers[index] = number;
+            }
+
+            string type = Console.ReadLine();
+
+            Predicate<int> predicate = null;
+
+            if (type == "even")
+            {
+                predicate = number => number % 2 == 0;
+            }
+            else if (type == "odd")
+            {
+                predicate = number => number % 2 != 0;
+            }
+
+            foreach (int number in numbers)
+            {
+                if (predicate(number))
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+        }
+    }
+}
---
+++ SoftUni Csharp Advanced/Functional Programming Exercise/P05.AppliedArithmetics/AppliedArithmetics.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P05.AppliedArithmetics
+{
+    class AppliedArithmetics
+    {
+        static void Main()
+        {
+            List<int> numbers = Console.ReadLine().Split().Select(int.Parse).ToList();
+
+            Action<List<int>> print = list => Console.WriteLine(string.Join(' ', list));
+
+            Func<List<int>, List<int>> applyCommands = null;
+
+            string command = string.Empty;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "add")
+                {
+                    applyCommands = list => list.Select(num => num += 1).ToList();
+                    numbers = applyCommands(numbers);
+                }
+                else if (command == "multiply")
+                {
+                    applyCommands = list => list.Select(num => num *= 2).ToList();
+                    numbers = applyCommands(numbers);
+                }
+                else if (command == "subtract")
+                {
+                    applyCommands = list => list.Select(num => num -= 1).ToList();
+                    numbers = applyCommands(numbers);
+                }
+                else if (command == "print")
+                {
+                    print(numbers);
+                }
+            }
+        }
+    }
+}
