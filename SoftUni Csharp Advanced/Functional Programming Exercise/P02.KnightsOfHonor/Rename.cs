a1d2fa6eed3a0f7d72668c46697cfc46c1d09842
Radoslav Gervaziev <radi7275@gmail.com>
2022-06-02 19:13:42 +0300

    Uploading solutions

---
+++ SoftUni Csharp Advanced/Functional Programming Exercise/P02.KnightsOfHonor/Rename.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace P02.KnightsOfHonor
+{
+    class Rename
+    {
+        static void Main()
+        {
+            string[] names = Console.ReadLine().Split();
+
+            Action<string> print = name => Console.WriteLine($"Sir {name}");
+
+            Array.ForEach(names, print);
+        }
+    }
+}
---
+++ SoftUni Csharp Advanced/Functional Programming Exercise/P03.CustomMinFunction/CustomFunction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace P03.CustomMinFunction
+{
+    class CustomFunction
+    {
+        static void Main()
+        {
+            int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Func<int[], int> getMin = numbers => numbers.Min();
+
+            Console.WriteLine(getMin(numbers));
+        }
+    }
+}
