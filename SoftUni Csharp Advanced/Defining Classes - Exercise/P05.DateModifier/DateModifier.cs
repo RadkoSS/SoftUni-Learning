2b77c6d6e9fed8025dfc91915caf464a998ecd77
Radoslav Gervaziev <radi7275@gmail.com>
2022-06-07 20:30:27 +0300

    Uploading solutions

---
+++ SoftUni Csharp Advanced/Defining Classes - Exercise/P05.DateModifier/DateModifier.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        private string day;
+
+        private string month;
+
+        private int year;
+
+        public string Day 
+        {
+            get { return day; }
+            set { day = value; }
+        }
+
+        public string Month
+        {
+            get { return month; }
+            set { month = value; }
+        }
+
+        public int Year 
+        {
+            get { return year; }
+            set { year = value; }
+        }
+
+    }
+}
