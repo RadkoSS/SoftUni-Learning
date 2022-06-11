1040bb745c94db1bcf09bc5e0b6d91f58f946295
Radoslav Gervaziev <radi7275@gmail.com>
2022-06-11 17:09:51 +0300

    Solved P04.GenericSwapMethodInteger

---
+++ SoftUni Csharp Advanced/Generics - Exercise SoftUni/P04.GenericSwapMethodInteger/Box.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace P04.GenericSwapMethodInteger
+{
+    public class Box<T>
+    {
+        public Box(List<T> value)
+        {
+            this.Value = value;
+        }
+
+        public List<T> Value { get; }
+
+        public void Swap(List<T> list, int firstIndex, int secondIndex)
+        {
+            if (!CheckIndices(list, firstIndex, secondIndex))
+                throw new ArgumentException($"{firstIndex} and {secondIndex} indexes are not presented in the list!");
+
+            var tempItem = list[firstIndex];
+
+            list[firstIndex] = list[secondIndex];
+
+            list[secondIndex] = tempItem;
+        }
+
+        private bool CheckIndices(List<T> list, int firstIndex, int secondIndex)
+        {
+            return firstIndex >= 0 && firstIndex < list.Count && secondIndex >= 0 && secondIndex < list.Count;
+        }
+
+        public override string ToString()
+        {
+            var programOutput = new StringBuilder();
+
+            this.Value.ForEach(inputLine => programOutput.AppendLine($"{inputLine.GetType()}: {inputLine}"));
+
+            return programOutput.ToString().TrimEnd();
+        }
+    }
+}
---
+++ SoftUni Csharp Advanced/Generics - Exercise SoftUni/P04.GenericSwapMethodInteger/StartUp.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P04.GenericSwapMethodInteger
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int boxesCount = int.Parse(Console.ReadLine());
+            List<int> numbers = new List<int>();
+
+            for (int i = 0; i < boxesCount; i++)
+            {
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            var indexesToSwap = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var box = new Box<int>(numbers);
+
+            box.Swap(numbers, indexesToSwap[0], indexesToSwap[1]);
+
+            Console.WriteLine(box);
+        }
+    }
+}
