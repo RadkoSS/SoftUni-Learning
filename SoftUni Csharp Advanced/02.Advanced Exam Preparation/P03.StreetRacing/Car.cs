47d2b93e5f8171b2eeb0b108ce6da98b5cadaf42
Radoslav Gervaziev <radi7275@gmail.com>
2022-06-20 20:31:41 +0300

    85/100

---
+++ SoftUni Csharp Advanced/02.Advanced Exam Preparation/P03.StreetRacing/Car.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+
+namespace StreetRacing
+{
+    public class Car : IComparable<Car>
+    {
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public string LicensePlate { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public double Weight { get; set; }
+
+        public Car(string make, string model, string licensePlate, int horsePower, double weight)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.LicensePlate = licensePlate;
+            this.HorsePower = horsePower;
+            this.Weight = weight;
+        }
+
+        public int CompareTo(Car other)
+        {
+            return this.HorsePower.CompareTo(other.HorsePower);
+        }
+
+        public override string ToString()
+        {
+            var outputText = new StringBuilder();
+
+            outputText.AppendLine($"Make: {this.Make}");
+            outputText.AppendLine($"Model: {this.Model}");
+            outputText.AppendLine($"LicensePlate: {this.LicensePlate}");
+            outputText.AppendLine($"HorsePower: {this.HorsePower}");
+            outputText.AppendLine($"Weight: {this.Weight}");
+
+            return outputText.ToString().TrimEnd();
+        }
+    }
+}
---
+++ SoftUni Csharp Advanced/02.Advanced Exam Preparation/P03.StreetRacing/Race.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        public List<Car> Participants { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Laps { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int MaxHorsePower { get; set; }
+
+        public Race()
+        {
+            this.Participants = new List<Car>();
+        }
+
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower) : this()
+        {
+            this.Name = name;
+            this.Type = type;
+            this.Laps = laps;
+            this.Capacity = capacity;
+            this.MaxHorsePower = maxHorsePower;
+        }
+
+        public int Count => this.Participants.Count;
+
+        public void Add(Car car)
+        {
+            var sameCarFound = this.Participants.Any(x => x.LicensePlate == car.LicensePlate);
+
+            if (!sameCarFound && car.HorsePower <= this.MaxHorsePower && this.Participants.Count < this.Capacity)
+            {
+                this.Participants.Add(car);
+            }
+        }
+
+        public bool Remove(string licensePlate)
+        {
+            var searchedCar = FindParticipant(licensePlate);
+
+            if (searchedCar != null)
+            {
+                this.Participants.Remove(searchedCar);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Car FindParticipant(string licencePlate) => this.Participants.FirstOrDefault(car => car.LicensePlate == licencePlate);
+
+        public Car GetMostPowerfulCar()
+        {
+            if (this.Participants.Count == 0)
+            {
+                return null;
+            }
+
+            if (this.Participants.Count == 1)
+            {
+                return this.Participants.First();
+            }
+
+            var mostPowerfulCar = this.Participants.First();
+
+            foreach (var car in this.Participants)
+            {
+                if (mostPowerfulCar.CompareTo(car) < 0)
+                {
+                    mostPowerfulCar = car;
+                }
+            }
+
+            return mostPowerfulCar;
+        }
+
+        public string Report()
+        {
+            var outputText = new StringBuilder();
+
+            outputText.AppendLine($"Race: {this.Name} - Type: {this.Type} (Laps: {this.Laps})");
+
+            if (this.Participants.Count > 0)
+            {
+                outputText.Append(string.Join(Environment.NewLine, this.Participants));
+            }
+            
+            return outputText.ToString().TrimEnd();
+        }
+    }
+}
