2bc43863f82d2435ade1fdb1610448d21ec74ebe
Radoslav Gervaziev <radi7275@gmail.com>
2022-06-22 12:34:18 +0300

    P01.WarmWinter and P02.SuperMario 100/100 solved

---
+++ SoftUni Csharp Advanced/04.Advanced Exam Preparation/P01.WarmWinter/StartUp.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace P01.WarmWinter
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            var hats = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var scarfs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var hatsStack = new Stack<int>(hats);
+
+            var scarfsQueue = new Queue<int>(scarfs);
+
+            var sets = new List<int>();
+
+            while (hatsStack.Count > 0 && scarfsQueue.Count > 0)
+            {
+                var currentHat = hatsStack.Pop();
+                var currentScarf = scarfsQueue.Peek();
+
+                if (currentHat > currentScarf)
+                {
+                    currentScarf += currentHat;
+                    scarfsQueue.Dequeue();
+
+                    sets.Add(currentScarf);
+
+                }
+                else if (currentHat == currentScarf)
+                {
+                    currentHat++;
+                    scarfsQueue.Dequeue();
+
+                    hatsStack.Push(currentHat);
+                }
+
+            }
+
+            Console.WriteLine($"The most expensive set is: {sets.Max()}");
+            Console.WriteLine(string.Join(' ', sets));
+        }
+    }
+}
---
+++ SoftUni Csharp Advanced/04.Advanced Exam Preparation/P02.SuperMario/StartUp.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace P02.SuperMario
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            int lives = int.Parse(Console.ReadLine());
+
+            int rowsOfCastle = int.Parse(Console.ReadLine());
+
+            char[][] castleMatrix = new char[rowsOfCastle][];
+
+            int superMarioRow = -1;
+
+            int superMarioColumn = -1;
+
+            InitializeCaste(ref castleMatrix, ref superMarioRow, ref superMarioColumn);
+
+            var isOver = false;
+            var princesSaved = false;
+
+            MoveInCastle(ref castleMatrix, ref isOver, ref princesSaved, ref superMarioRow, ref superMarioColumn, ref lives);
+
+            PrintResult(castleMatrix, isOver, princesSaved, superMarioRow, superMarioColumn, lives);
+            
+        }
+
+        static void InitializeCaste(ref char[][] castleMatrix, ref int superMarioRow, ref int superMarioColumn)
+        {
+            for (int row = 0; row < castleMatrix.GetLength(0); row++)
+            {
+                var line = Console.ReadLine();
+
+                if (line.Contains('M'))
+                {
+                    superMarioRow = row;
+                    superMarioColumn = line.IndexOf('M');
+                }
+
+                castleMatrix[row] = line.ToCharArray();
+            }
+        }
+
+        static void MoveInCastle(ref char[][] castleMatrix, ref bool isOver, ref bool princesSaved, ref int superMarioRow, ref int superMarioColumn, ref int lives)
+        {
+            while (!isOver)
+            {
+                string[] command = Console.ReadLine().Split();
+
+                char move = char.Parse(command[0]);
+
+                int spawnRow = int.Parse(command[1]);
+
+                int spawnCol = int.Parse(command[2]);
+
+                castleMatrix[spawnRow][spawnCol] = 'B';
+
+                if (move == 'W' && superMarioRow - 1 >= 0)
+                {
+                    castleMatrix[superMarioRow][superMarioColumn] = '-';
+                    superMarioRow--;
+                }
+
+                else if (move == 'S' && superMarioRow + 1 < castleMatrix.GetLength(0))
+                {
+                    castleMatrix[superMarioRow][superMarioColumn] = '-';
+                    superMarioRow++;
+                }
+
+                else if (move == 'A' && superMarioColumn - 1 >= 0)
+                {
+                    castleMatrix[superMarioRow][superMarioColumn] = '-';
+                    superMarioColumn--;
+                }
+
+                else if (move == 'D' && superMarioColumn + 1 < castleMatrix[superMarioRow].Length)
+                {
+                    castleMatrix[superMarioRow][superMarioColumn] = '-';
+                    superMarioColumn++;
+                }
+
+                lives--;
+
+                if (lives <= 0)
+                {
+                    castleMatrix[superMarioRow][superMarioColumn] = 'X';
+
+                    isOver = true;
+                }
+
+                if (castleMatrix[superMarioRow][superMarioColumn] == 'B')
+                {
+                    lives -= 2;
+                    if (lives <= 0)
+                    {
+                        castleMatrix[superMarioRow][superMarioColumn] = 'X';
+
+                        isOver = true;
+                    }
+                }
+
+                else if (castleMatrix[superMarioRow][superMarioColumn] == 'P')
+                {
+                    castleMatrix[superMarioRow][superMarioColumn] = '-';
+                    princesSaved = true;
+
+                    isOver = true;
+                }
+
+            }
+
+        }
+
+        static void PrintResult(char[][] matrix, bool isOver, bool princesSaved, int superMarioRow, int superMarioColumn, int lives)
+        {
+            if (isOver && princesSaved)
+            {
+                Console.WriteLine($"Mario has successfully saved the princess! Lives left: {lives}");
+            }
+
+            else if (isOver)
+            {
+                Console.WriteLine($"Mario died at {superMarioRow};{superMarioColumn}.");
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                Console.WriteLine(string.Join(string.Empty, matrix[row]));
+            }
+        }
+    }
+}
