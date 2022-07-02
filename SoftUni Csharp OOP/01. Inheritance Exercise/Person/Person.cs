896c8bda4524906ec3e26869d40ac02a0c9b58ab
Radoslav Gervaziev <radi7275@gmail.com>
2022-07-02 19:46:02 +0300

    Solved 01. and 02.

---
+++ SoftUni Csharp OOP/01. Inheritance Exercise/Person/Person.cs
@@ -0,0 +1,40 @@
+﻿namespace Person
+{
+    public class Person
+    {
+        private string _name;
+
+        private int age;
+
+        public string Name
+        {
+            get { return _name; }
+
+            set { _name = value; }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value < 0)
+                    age = 0;
+
+                else
+                    age = value;
+            }
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {Name}, Age: {Age}";
+        }
+    }
+}
--- SoftUni Csharp OOP/01. Inheritance Exercise/Person/StartUp.cs
+++ SoftUni Csharp OOP/01. Inheritance Exercise/Person/StartUp.cs
@@ -1,9 +1,16 @@
 ﻿namespace Person
 {
+    using System;
+
     public class StartUp
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+            string name = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            Child child = new Child(name, age);
+            Console.WriteLine(child);
 
         }
     }
