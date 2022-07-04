8073c5ead8505457fd8225de5febe971dbd1761e
Radoslav Gervaziev <radi7275@gmail.com>
2022-07-04 12:47:34 +0300

    Solved "Animals" problem

---
+++ SoftUni Csharp OOP/01. Inheritance Exercise/Animals/Animal.cs
@@ -0,0 +1,84 @@
+﻿namespace Animals
+{
+    using System;
+    using System.Text;
+
+    public abstract class Animal
+    {
+        private string name;
+
+        private int age;
+
+        private string gender;
+
+        protected Animal(string name, int age, string gender)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Gender = gender;
+        }
+
+        public string Name 
+        {
+            get
+            {
+                return name; 
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(nameof(this.Name), "Name must not be null, empty or white-space!");
+                }
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get 
+            { 
+                return age;
+            }
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(nameof(this.Age), "Age must not be a negative number!");
+                }
+                age = value;
+            }
+        }
+
+        public string Gender
+        {
+            get 
+            { 
+                return gender;
+            }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(nameof(this.Gender), "Gender must not be null, empty or white-space!");
+                }
+                gender = value;
+            }
+        }
+
+        public abstract string ProduceSound();
+
+        public override string ToString()
+        {
+            var textOutput = new StringBuilder();
+
+            textOutput.AppendLine(this.GetType().Name.ToString());
+
+            textOutput.AppendLine($"{this.Name} {this.Age} {this.Gender}");
+
+            textOutput.Append(ProduceSound());
+
+            return textOutput.ToString().TrimEnd();
+        }
+    }
+}
--- SoftUni Csharp OOP/01. Inheritance Exercise/Animals/StartUp.cs
+++ SoftUni Csharp OOP/01. Inheritance Exercise/Animals/StartUp.cs
@@ -1,10 +1,67 @@
 ﻿namespace Animals
 {
+    using System;
+    using System.Collections.Generic;
+
     public class StartUp
     {
         public static void Main()
         {
-            
+            var animals = new List<Animal>();
+
+            var animalType = string.Empty;
+
+            while ((animalType = Console.ReadLine()) != "Beast!")
+            {
+                try
+                {
+                    var animalInfo = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                    var animalName = animalInfo[0];
+                    var animalAge = int.Parse(animalInfo[1]);
+                    var animalGender = animalInfo[2];
+
+                    Animal animal = null;
+
+                    if (animalType == "Dog")
+                    {
+                        animal = new Dog(animalName, animalAge, animalGender);
+                    }
+
+                    else if (animalType == "Cat")
+                    {
+                        animal = new Cat(animalName, animalAge, animalGender);
+                    }
+
+                    else if (animalType == "Frog")
+                    {
+                        animal = new Cat(animalName, animalAge, animalGender);
+                    }
+
+                    else if (animalType == "Kitten")
+                    {
+                        animal = new Kitten(animalName, animalAge);
+                    }
+
+                    else if (animalType == "Tomcat")
+                    {
+                        animal = new Tomcat(animalName, animalAge);
+                    }
+
+                    else
+                    {
+                        throw new Exception("Invalid type of animal!");
+                    }
+
+                    animals.Add(animal);
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, animals));
         }
     }
 }
