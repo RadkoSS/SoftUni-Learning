ad3824deb10058867268b78cf9ec5ddb298c5405
Radoslav Gervaziev <radi7275@gmail.com>
2022-07-06 21:04:27 +0300

    Made a change to StartUp.cs

--- SoftUni Csharp OOP/02. Encapsulation Exercise/P01.ClassBoxData/StartUp.cs
+++ SoftUni Csharp OOP/02. Encapsulation Exercise/P01.ClassBoxData/StartUp.cs
@@ -2,11 +2,11 @@
 
 namespace P01.ClassBoxData
 {
-    internal class Program
+    public class StartUp
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
