a9aa931f3b35aff9f26bbcffdbba03964dd72b08
Radoslav Gervaziev <radi7275@gmail.com>
2022-07-10 17:08:40 +0300

    P01. solved

---
+++ SoftUni Csharp OOP/04. Interfaces and Abstraction - Exercise/P01.DefineAnInterfaceIPerson/Citizen.cs
@@ -0,0 +1,52 @@
+﻿namespace PersonInfo
+{
+    using System;
+
+    public class Citizen : IPerson
+    {
+        private string name;
+
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name 
+        {
+            get => this.name;
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new Exception($"{nameof(this.Name)} Name cannot be null, empty or whitespace.");
+                }
+                this.name = value;
+            }
+        
+        }
+
+        public int Age 
+        {
+            get => this.age;
+
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new Exception($"{nameof(this.Age)} Age cannot be zero or a negative number.");
+                }
+                this.age = value;
+            }
+        
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Name} is {this.Age}yo.";
+        }
+    }
+}
---
+++ SoftUni Csharp OOP/04. Interfaces and Abstraction - Exercise/P01.DefineAnInterfaceIPerson/StartUp.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonInfo
+{
+    using System;
+
+    public class StartUp
+    {
+        static void Main()
+        {
+            IPerson person = new Citizen("Radko", 18);
+
+            Console.WriteLine(person.Name);
+            Console.WriteLine(person.Age);
+        }
+    }
+}
