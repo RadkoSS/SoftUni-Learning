4c262029da392581fbfb4344ec25d2a7d3881ad4
Radoslav Gervaziev <radi7275@gmail.com>
2022-08-03 00:20:39 +0300

    Did some more changes.

--- SoftUni Csharp OOP/09. CSharp OOP Exam Preparation/Formula1/Core/Controller.cs
+++ SoftUni Csharp OOP/09. CSharp OOP Exam Preparation/Formula1/Core/Controller.cs
@@ -3,8 +3,6 @@
     using System;
     using System.Linq;
     using System.Text;
-    using System.Xml.Linq;
-    using System.Diagnostics;
     using System.Collections.Generic;
 
     using Models;
