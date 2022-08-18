b76e8dddf7a73c8b2eb63a1fa21676fef56aaef4
Radoslav Gervaziev <radi7275@gmail.com>
2022-08-18 17:18:01 +0300

    P02.BlackBoxInteger is solved 100/100.

--- SoftUni Csharp OOP/14. Reflection Archived Exercise/P02.BlackBoxInteger/BlackBoxIntegerTests.cs
+++ SoftUni Csharp OOP/14. Reflection Archived Exercise/P02.BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -1,12 +1,43 @@
 ﻿namespace P02_BlackBoxInteger
 {
     using System;
+    using System.Linq;
+    using System.Reflection;
 
     public class BlackBoxIntegerTests
     {
         public static void Main()
         {
-            //TODO put your reflection code here
+            Type type = typeof(BlackBoxInteger);
+
+            FieldInfo field = type.GetFields(BindingFlags.Instance | BindingFlags.NonPublic).FirstOrDefault(x => x.Name == "innerValue");
+
+            MethodInfo[] methods = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            ConstructorInfo[] constructors = type.GetConstructors(BindingFlags.Instance | BindingFlags.NonPublic);
+
+            ConstructorInfo constructorWithParameter = constructors.FirstOrDefault(x => x.GetParameters().Length == 1);
+
+            BlackBoxInteger bbi = constructorWithParameter?.Invoke(new object[] { 0 }) as BlackBoxInteger;
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] args = command?.Split('_');
+
+                string methodName = args?[0];
+
+                int value = int.Parse(args![1]);
+
+                MethodInfo searchedMethod = methods.FirstOrDefault(x => x.Name == methodName);
+
+                searchedMethod?.Invoke(bbi, new object[] { value });
+
+                int currentValueOfField = (int) field?.GetValue(bbi)!;
+
+                Console.WriteLine(currentValueOfField);
+            }
         }
     }
 }
