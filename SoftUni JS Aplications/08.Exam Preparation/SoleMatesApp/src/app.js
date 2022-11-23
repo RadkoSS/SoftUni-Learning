d5813b917521a124c23045a17eb928e18940c683
Radoslav Gervaziev <radi7275@gmail.com>
2022-11-23 22:43:57 +0200

    Initial commit of the app's architecture.

---
+++ SoftUni JS Aplications/08.Exam Preparation/SoleMatesApp/src/app.js
@@ -0,0 +1,6 @@
+import {html, render} from './utils/lib.js';
+
+const mainRoot = document.getElementsByTagName('main')[0];
+const navRoot = document.querySelector('navigation');
+
+console.log('i am working');
