2f0e75d7c0f4618b0ea5b1c23359d3494a589818
Radoslav Gervaziev <radi7275@gmail.com>
2023-03-07 23:01:16 +0200

    Commit

---
+++ SoftUni Csharp Entity Framework Core/06.Advanced Querying Exercise/BookShop/StartUp.cs
@@ -0,0 +1,40 @@
+﻿namespace BookShop;
+
+using System.Linq;
+
+using Data;
+using Initializer;
+using Models.Enums;
+
+public class StartUp
+{
+    public static void Main()
+    {
+        using var dbContext = new BookShopContext();
+        //DbInitializer.ResetDatabase(db);
+
+        string result = GetBooksByAgeRestriction(dbContext, Console.ReadLine()!);
+
+        Console.WriteLine(result);
+    }
+        
+    public static string GetBooksByAgeRestriction(BookShopContext dbContext, string command)
+    {
+        try
+        {
+            AgeRestriction ageRestriction = Enum.Parse<AgeRestriction>(command, true);
+
+            var result = dbContext.Books
+                .Where(b => b.AgeRestriction == ageRestriction)
+                .Select(b => b.Title)
+                .OrderBy(title => title)
+                .ToList();
+
+            return string.Join(Environment.NewLine, result);
+        }
+        catch (Exception exception)
+        {
+            return exception.Message;
+        }
+    }
+}
