3771524dd6012cada68d3559b4b183e1d9540dd1
Radoslav Gervaziev <radi7275@gmail.com>
2023-05-29 20:15:44 +0300

    Initial commit. Trying out Vanilla JS in the home page.

---
+++ SoftUni Csharp Web Fund/01.ASPDemoExercises/MVCIntroDemo/MVCIntroDemo/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿namespace MVCIntroDemo.Controllers;
+
+using System.Diagnostics;
+
+using Microsoft.AspNetCore.Mvc;
+
+using Models;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        ViewData["Message"] = "Hello world!";
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ SoftUni Csharp Web Fund/01.ASPDemoExercises/MVCIntroDemo/MVCIntroDemo/wwwroot/js/site.js
@@ -0,0 +1,13 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+const btn = document.getElementById('toggle');
+const containerToToggle = document.getElementById('container');
+
+btn.addEventListener('click', toggleVisibility);
+
+function toggleVisibility() {
+    const currentState = containerToToggle.style.display;
+    containerToToggle.style.display = currentState == 'none' ? 'block' : 'none';
+}
