07823b8ebb3f454907d96d95aa3895dcbd0cd6a4
Radoslav Gervaziev <radi7275@gmail.com>
2023-09-25 22:25:10 +0300

    Initial commit

---
+++ SoftUni Csharp DS and Algorithms/01.Sorting Algorithms and Recursion - Exercise/P01.ReverseArray/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace P01.ReverseArray;
+
+internal class Program
+{
+    private static string[] numbersArray;
+    private static string[] reversedNumbersArray;
+
+    private static void Main()
+    { 
+        numbersArray = Console.ReadLine().Split(' ');
+            
+        reversedNumbersArray = new string[numbersArray.Length];
+
+        ReverseArray(0);
+    }
+
+    private static void ReverseArray(int currentIndex)
+    {
+        if (currentIndex < 0 || currentIndex >= numbersArray.Length)
+        {
+            PrintResult();
+            return;
+        }
+
+        reversedNumbersArray[numbersArray.Length - 1 - currentIndex] = numbersArray[currentIndex];
+
+        ReverseArray(++currentIndex);
+    }
+
+    private static void PrintResult()
+        => Console.WriteLine(string.Join(' ', reversedNumbersArray));
+}
---
+++ SoftUni Csharp DS and Algorithms/01.Sorting Algorithms and Recursion - Exercise/P02.ToRecur/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace P02.ToRecur;
+
+internal class Program
+{
+    private static void Main()
+    {
+
+    }
+}
